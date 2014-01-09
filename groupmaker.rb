f0ad316d682bd6047453b667dcd0bcc6ad06308b
David Markowitz <microfuzz@mac.com>
2014-01-09 14:35:55 -0500

    my logic outlined in the file

--- groupmaker.rb
+++ groupmaker.rb
@@ -1,1 +1,12 @@
 people = ['Adi', 'Adrian', 'Alex', 'Anthony', 'Ben', 'Brian', 'Bryan', 'Chloe', 'Eric', 'Jane', 'Jeff', 'Jillian', 'Joe', 'Luke', 'Matt', 'Nick', 'Nicky', 'Ralph', 'Ron', 'Ryan', 'Sandip', 'Sean', 'Shefali', 'Simon', 'Stephen']
+
+puts "How large is your group?"
+gets.chomp
+
+#then if gets.chomp > array size says
+
+"Too large a number. This group is only {Prints array size}. Please select another number of groups"
+
+#then splits shuffles the array
+#then puts the randomly shuffled names into groups
+#converts the results of those array into a string
