45922ffa19e4b4583632b4ddf4cf97f5d421bd17
Mike Stone <mikestone14@gmail.com>
2014-01-09 14:36:50 -0500

    refactored code to delete an uneccessary line

--- groupmaker.rb
+++ groupmaker.rb
@@ -1,15 +1,14 @@
 people = ['Adi', 'Adrian', 'Alex', 'Anthony', 'Ben', 'Brian', 'Bryan', 'Chloe', 'Eric', 'Jane', 'Jeff', 'Jillian', 'Joe', 'Luke', 'Matt', 'Nick', 'Nicky', 'Ralph', 'Ron', 'Ryan', 'Sandip', 'Sean', 'Shefali', 'Simon', 'Stephen']
 people = people.shuffle
 
-puts "What size groups would you like?"
+print "What size groups would you like? "
 n = gets.chomp.to_i
 
 array_groups = people.each_slice(n).to_a
-
 large_array = [] + array_groups
 
 large_array.each.with_index do |group, index|
 	index += 1
-	group_string = group.join(', ')
-	puts "Group ##{index}: #{group_string}"
+	group.join(', ')
+	puts "Group ##{index}: #{group}"
 end
