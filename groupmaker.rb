4c3d8b980d2e55a92f35edca2594836344076a58
tsyan <anna.tsykalova@gmail.com>
2014-01-09 14:32:22 -0500

    doesn't work yet

--- groupmaker.rb
+++ groupmaker.rb
@@ -1,1 +1,14 @@
 people = ['Adi', 'Adrian', 'Alex', 'Anthony', 'Ben', 'Brian', 'Bryan', 'Chloe', 'Eric', 'Jane', 'Jeff', 'Jillian', 'Joe', 'Luke', 'Matt', 'Nick', 'Nicky', 'Ralph', 'Ron', 'Ryan', 'Sandip', 'Sean', 'Shefali', 'Simon', 'Stephen']
+
+# ask for how large of a group
+# create random groups
+# any leftovers go to the last group
+
+def groupmaker(group_size, people)
+	group_array = people.each_slice(group_size).to_a
+	return group_array
+end
+
+puts groupmaker(3, people)
+
+# nope not yet
