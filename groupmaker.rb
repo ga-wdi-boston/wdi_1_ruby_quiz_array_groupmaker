e063ca3bb1d1e0c82755cab1cf3e77c7e55d216f
K Mak <karen.mak@stx.oxon.org>
2014-01-09 14:31:46 -0500

    not random

--- groupmaker.rb
+++ groupmaker.rb
@@ -1,1 +1,8 @@
 people = ['Adi', 'Adrian', 'Alex', 'Anthony', 'Ben', 'Brian', 'Bryan', 'Chloe', 'Eric', 'Jane', 'Jeff', 'Jillian', 'Joe', 'Luke', 'Matt', 'Nick', 'Nicky', 'Ralph', 'Ron', 'Ryan', 'Sandip', 'Sean', 'Shefali', 'Simon', 'Stephen']
+
+
+# for how large of a group, and then it creates random groups. If anyone is left over, just throw them into the last group
+
+people.each_slice(2) do |x,y|
+  p [x,y,z]
+end
