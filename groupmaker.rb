23d01737d5ee4bb4ad3e26cc44dbf4abe77e9316
Jordan Morano <jordan.morano@gmail.com>
2014-01-09 14:36:34 -0500

    shuffle was in my cards..

--- groupmaker.rb
+++ groupmaker.rb
@@ -1,1 +1,11 @@
 people = ['Adi', 'Adrian', 'Alex', 'Anthony', 'Ben', 'Brian', 'Bryan', 'Chloe', 'Eric', 'Jane', 'Jeff', 'Jillian', 'Joe', 'Luke', 'Matt', 'Nick', 'Nicky', 'Ralph', 'Ron', 'Ryan', 'Sandip', 'Sean', 'Shefali', 'Simon', 'Stephen']
+
+## input group size
+
+puts "How large of a group?"
+size = 5
+num_groups = (people.length/size).to_i
+
+p num_groups
+
+
