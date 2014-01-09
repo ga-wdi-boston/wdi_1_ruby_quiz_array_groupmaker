d6699f70c7dd69c63e74c14f43cf6a3a7aa40370
Janice Smith <janice.k.n.smith@gmail.com>
2014-01-09 14:33:24 -0500

    here

--- groupmaker.rb
+++ groupmaker.rb
@@ -1,1 +1,27 @@
 people = ['Adi', 'Adrian', 'Alex', 'Anthony', 'Ben', 'Brian', 'Bryan', 'Chloe', 'Eric', 'Jane', 'Jeff', 'Jillian', 'Joe', 'Luke', 'Matt', 'Nick', 'Nicky', 'Ralph', 'Ron', 'Ryan', 'Sandip', 'Sean', 'Shefali', 'Simon', 'Stephen']
+
+
+# I just need to have it ask me for
+#  how large of a group, and then it creates
+#  random groups. If anyone is left over, just throw them into the last group. But I think you can do this easily, so can you get this done in say... the next 7 minutes for me?"
+
+# There are no RSpec/Travis tests setup
+# for this quiz. Put your code in the included
+
+
+
+
+#user input
+def get_input(prompt)
+	puts prompt
+	gets.chomp
+end
+
+number_of_groups = get_input( "how many groups?")
+number_of_people = get_input( "how many people?")
+
+
+groups = people [rand(10)]+ " " + people [rand(9)]+ " " + people [rand(8)]
+
+puts groups.inspect
+
